e27f93b5ee4164b6a8b45f09c489f54a8b2c7439
Djosef0 <youssefbensaid827@gmail.com>
2023-03-05 17:58:58 +0100

    edit function add msg

--- controllers/Msg.js
+++ controllers/Msg.js
@@ -2,41 +2,42 @@ import { User } from "../models/users.js";
 import {Msg} from "../models/msg.js";
 import { Produit } from "../models/produits.js";
 
-export const addMessage = async(req , res)=>{
+export const addMessage = async (req, res) => {
     const content = req.body;
     const user = req.params.id;
     const to = req.params.idtouser;
     const product = req.params.idproduct;
-    
-try{
-    
-    const NewMessage = new Msg(
-        content,
-    )
-    const messageEnvoyer = await NewMessage.save();
-    res.status(200).json(messageEnvoyer)
-    
-
-    try{
-    await User.findByIdAndUpdate(user , {
-        $push : {msg : messageEnvoyer._id}
-    })
-    await User.findByIdAndUpdate(to , {
-        $push : {msg : messageEnvoyer._id}
-    })
-
-    await Produit.findByIdAndUpdate(product , {
-        $push : {msgUser : messageEnvoyer._id}
-    })
-   
-    }catch(error){
-        res.status(500).json({message : error.message})
+  
+    try {
+      const productFind = await Produit.findById(product);
+  
+      const NewMessage = new Msg(content);
+      const messageEnvoyer = await NewMessage.save();
+      res.status(200).json(messageEnvoyer);
+  
+      try {
+        await User.findByIdAndUpdate(user, {
+          $push: { msg: messageEnvoyer._id },
+        });
+        await User.findByIdAndUpdate(to, {
+          $push: { msg: messageEnvoyer._id },
+        });
+  
+        const index = productFind.msgUser.indexOf(user);
+        if (index === -1) {
+          await Produit.findByIdAndUpdate(product, {
+            $push: { msgUser: user },
+          });
+        }
+  
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-}catch(error){
-    res.status(500).json({message : error.message})
-}
-
-}
+  };
+  
 
 
 export const getConversation = async (req , res)=>{
@@ -71,8 +72,35 @@ export const getConversation = async (req , res)=>{
     }
 }
 
-export const Notification = async(req , res) => {
-   
-    
+export const Notification = async (req, res) => {
+    const id = req.params.id;
+    const me = await User.findById(id);
+    const listMessage = [];
+    const listProduit = [];
+    const userSendMe = [];
+    const finalResult = []
   
+    listMessage.push(me.msg);
+    for (let i = 0; i < me.produit.length; i++) {
+      const produit = await Produit.findById(me.produit[i]);
+      if (produit) {
+        listProduit.push(produit);
+        for (let j = 0; j < produit.msgUser.length; j++) {
+          const user = await User.findById(produit.msgUser[j]);
+          if (user) {
+            userSendMe.push(user);
+          }
+        }
+      }
+    }
+
+    for (let i = 0; i < userSendMe.msg.length; i++) {
+        for (let j = 0; j < userSendMe.msg.length; j++) {
+            if (me.msg.includes(userSendMe[i].msg[j])) {
+                finalResult.push(await Msg.findById(userSendMe[i].msg[j]))
+            }
+        }
+    }
+  
+    res.status(200).json(finalResult);
 }
