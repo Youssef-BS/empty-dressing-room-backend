b54229ddc90ae8d2d5ed222fa30540d9b8bed257
Djosef0 <youssefbensaid827@gmail.com>
2023-05-22 16:17:59 +0100

    delete 6 function in produit.js

--- routers/Produit.js
+++ routers/Produit.js
@@ -2,12 +2,6 @@ import  express from "express";
 import { addProduct ,
      getAllProduct ,
      getProductUser ,
-     getProductMen ,
-     getProductWomen,
-     getProductKids ,
-     getProductElectronique , 
-     getProductHome ,
-     getProductAnimaux,
      selectProduct,
      myProduits,
      updateProduit,
@@ -31,12 +25,6 @@ const router = express.Router();
  router.route("/addproduct/:id").post(addProduct);
  router.route("/").get(getAllProduct);
  router.route("/:id").get(getProductUser);
- router.route("/men/clother").get(getProductMen);
- router.route("/women/clother").get(getProductWomen);
- router.route("/kids/clother").get(getProductKids);
- router.route("/electronique/accesoire").get(getProductElectronique);
- router.route("/home/accesoire").get(getProductHome);
- router.route("/animaux/accesoire").get(getProductAnimaux);
  router.route("/select/:idproduct").get(selectProduct);
  router.route("/myproduit/test/:id/:idproduct").get(myProduits);
  router.route("/update/:idproduct").put(updateProduit);
